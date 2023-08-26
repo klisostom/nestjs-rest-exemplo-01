7f8f731c845068d9f46b14334bb294e7533f1f69
klisostom <klis@pm.me>
2023-08-26 20:05:58 -0300

    config

--- src/config/postgres.config.service.ts
+++ src/config/postgres.config.service.ts
@@ -1,14 +1,19 @@
+import { ConfigService } from "@nestjs/config";
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
 
 export class PostgresConfigService implements TypeOrmOptionsFactory {
+  constructor(private config: ConfigService) {
+
+  }
+
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'root',
-      password: 'root',
-      database: 'postgres',
+      host: this.config.get<string>('DB_HOST'),
+      port: this.config.get<number>('DB_PORT'),
+      username: this.config.get<string>('DB_USER'),
+      password: this.config.get<string>('DB_PASSWORD'),
+      database: this.config.get<string>('DB_DATABASE'),
       entities: [],
       synchronize: true,
     };
